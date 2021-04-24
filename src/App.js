979f2601f711426096c640448a4a9d7ad8868592
Sarthak Sharma <sarthaksharma_ma@srmuniv.edu.in>
2021-04-24 16:49:57 +0530

    done with video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
+import ImageGrid from './comps/imageGrid';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
+import Modal from './comps/modal';
 
 function App() {
+
+  const[selectedImg, setSelectedImg] =useState(null);
+
   return (
     <div className="App">
       <Title/>
       <UploadForm/>
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/>}
     </div>
   );
 }
